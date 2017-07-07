8caa98ed61eb8c1d320d864eebe2923741a7697b
mognoose <mognoose@gmail.com>
2017-07-07 11:01:38 +0300

    add header, footer, css and js

---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <title>Yritystuki</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" >
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
+<div id="container">
+    <div id="header">
+        <div id="logo"><h1>Yritystuki.fi</h1></div>
+        <div id="menu">
+            <ul>
+                <li>Etusivu</li>
+            </ul>
+            <ul>
+                <li>Demo / palvelu</li>
+            </ul>
+            <ul>
+                <li>Tukien myöntäjät</li>
+            </ul>
+            <ul>
+                <li>Ohjeita tukien hakuun</li>
+            </ul>
+            <ul>
+                <li>Osta</li>
+            </ul>
+            <ul>
+                <li>Rekisteröidy</li>
+            </ul>
+            <ul>
+                <li>Kirjaudu</li>
+            </ul>
+        </div>
+
+    </div>
--- index.php
+++ index.php
@@ -1,15 +1,6 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<title>Yritystuki</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" >
-<link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
-<body>
+<?php include('header.php'); ?>
 
 <h1>Yritystuki.fi</h1>
 
 
-</body>
-</html>
+
